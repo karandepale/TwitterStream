409277518d62e801805775e2810609cbdf78a5a9
locobuzz-karan <karan.depale@locobuzz.com>
2025-05-27 14:44:18 +0530

    [Backend]: Login controller created.

---
+++ backend/WebApi/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpGet("Test")]
+        public async Task<IActionResult> Test()
+        {
+            return Ok(new
+            {
+                message = $"Dummy test ",
+                url = "Google.com",
+                JWTToken = "",
+                Status = true
+            });
+        }
+
+    }
+}
